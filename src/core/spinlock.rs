4c1a32b5d034e39769a897b496e2484857774417
condy <condy0919@gmail.com>
2019-07-06 05:27:19 +0800

    spinlock tests

--- src/core/spinlock.rs
+++ src/core/spinlock.rs
@@ -1,3 +1,37 @@
+//! A spinlock & recursive spinlock.
+//!
+//! The `SpinLock&RecursiveSpinLock` provides the same interfaces except `is_locked`.
+//!
+//! The basic lock primitives:
+//!
+//! - `lock`
+//! - `unlock`
+//! - `trylock`
+//!
+//! # Example
+//! ```
+//! extern crate dpdk;
+//!
+//! use dpdk::core::spinlock;
+//!
+//! # fn main() {
+//! let mut lk = spinlock::SpinLock::default();
+//! let mut val = 0;
+//!
+//! lk.lock();
+//! val += 1;
+//! lk.unlock();
+//!
+//! assert_eq!(val, 1);
+//!
+//! # }
+//! ```
+//!
+//! NOTE:
+//! [TSX](https://gcc.gnu.org/onlinedocs/gcc-4.8.2/gcc/X86-transactional-memory-intrinsics.html) supports will be available whem `asm!` is stable.
+//!
+//!
+
 use std::sync::atomic::{spin_loop_hint, AtomicBool, Ordering};
 use super::gettid;
 
@@ -121,5 +155,55 @@ impl RecursiveSpinLock {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
+    #[test]
+    #[should_panic]
+    fn spinlock_drop_when_locked() {
+        let mut lk = SpinLock::default();
+
+        lk.lock();
+    }
+
+    #[test]
+    fn spinlock_lock_unlock() {
+        let mut lk = SpinLock::default();
+        assert!(!lk.is_locked());
+
+        lk.lock();
+        assert!(lk.is_locked());
+
+        lk.unlock();
+        assert!(!lk.is_locked());
+    }
+
+    #[test]
+    fn spinlock_trylock() {
+        let mut lk = SpinLock::default();
+        assert!(!lk.is_locked());
+
+        assert!(lk.trylock());
+        assert!(lk.is_locked());
+
+        assert!(!lk.trylock());
+
+        lk.unlock();
+        assert!(!lk.is_locked());
+    }
+
+    #[test]
+    #[should_panic]
+    fn recursive_spinlock_drop_when_locked() {
+        let mut lk = RecursiveSpinLock::default();
+
+        lk.lock();
+    }
+
+    #[test]
+    fn recursive_spinlock_lock_unlock() {
+        let mut lk = RecursiveSpinLock::default();
+
+        lk.lock();
+        lk.unlock();
+    }
 }
